41de9245fac48dd3e6728cade435e6da62f55ca9
Luisse M. Barcelona <barcelonalouie13@gmail.com>
2026-05-17 15:26:12 +0800

    2nd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 
 // ADAFRUIT IO CREDENTIALS
 const AIO_USERNAME = "barce";
-const AIO_KEY = "aio_gdqP26j5BmJTsG3xnQdHHVVIan7p";
+const AIO_KEY = "aio_nqNm75Atva25HzDL7LKaX0XjdmRH";
 
 // SYSTEM VARIABLES
 let usageCount = 0;
